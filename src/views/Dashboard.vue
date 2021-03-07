b625618c0eecf38971b4fc1de292a665c53127ea
Kiet Pham <kietpham.dev@gmail.com>
2021-03-08 00:10:46 +1100

    Directives

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -29,6 +29,22 @@
       </v-col>
     </v-row>
 
+    <v-row id="below-the-fold" v-intersect="showMoreContent">
+      <v-col cols="12" md="8">
+        <EmployeesTable :employees="employees" @select-employee="setEmployee" />
+      </v-col>
+      <v-col cols="12" md="4">
+        <EventTimeline :timeline="timeline" />
+      </v-col>
+    </v-row>
+
+    <v-row v-if="loadNewContent" id="more-content">
+      <v-col>
+        <v-skeleton-loader ref="skeleton" type="table" class="mx-auto">
+        </v-skeleton-loader>
+      </v-col>
+    </v-row>
+
     <v-snackbar v-model="snackbar" :left="$vuetify.breakpoint.lgAndUp">
       You have selected {{ selectedEmployee.name }},
       {{ selectedEmployee.title }}
@@ -40,15 +56,15 @@
 </template>
 
 <script>
-import EmployeesTable from '../components/EmployeesTable'
-import EventTimeline from '../components/EventTimeline'
-import SalesGraph from '../components/SalesGraph'
-import StatisticCard from '../components/StatisticCard'
+import EmployeesTable from '../components/EmployeesTable';
+import EventTimeline from '../components/EventTimeline';
+import SalesGraph from '../components/SalesGraph';
+import StatisticCard from '../components/StatisticCard';
 
-import employeesData from '../data/employees.json'
-import timelineData from '../data/timeline.json'
-import salesData from '../data/sales.json'
-import statisticsData from '../data/statistics.json'
+import employeesData from '../data/employees.json';
+import timelineData from '../data/timeline.json';
+import salesData from '../data/sales.json';
+import statisticsData from '../data/statistics.json';
 
 export default {
   name: 'DashboardPage',
@@ -60,6 +76,7 @@ export default {
   },
   data() {
     return {
+      loadNewContent: false,
       employees: employeesData,
       sales: salesData,
       selectedEmployee: {
@@ -69,14 +86,17 @@ export default {
       snackbar: false,
       statistics: statisticsData,
       timeline: timelineData
-    }
+    };
   },
   methods: {
     setEmployee(event) {
-      this.snackbar = true
-      this.selectedEmployee.name = event.name
-      this.selectedEmployee.title = event.title
+      this.snackbar = true;
+      this.selectedEmployee.name = event.name;
+      this.selectedEmployee.title = event.title;
+    },
+    showMoreContent(entries) {
+      this.loadNewContent = entries[0].isIntersecting;
     }
   }
-}
+};
 </script>
