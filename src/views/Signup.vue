59fdeb92236ac52dfa42dea9f15685e0fd95a7d1
Kiet Pham <kietpham.dev@gmail.com>
2021-03-07 23:03:54 +1100

    Forms: Fundamentals

---
+++ src/views/Signup.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container class="">
+    <v-row>
+      <v-col>
+        <h1>Signup</h1>
+        <v-form>
+          <v-text-field
+            type="email"
+            error-count=""
+            placeholder="Email"
+            label=""
+            append-icon=""
+            v-model="email"
+          ></v-text-field>
+          <v-autocomplete
+            label="Which browser do you use?"
+            :items="browsers"
+          ></v-autocomplete>
+          <v-file-input
+            accept="image/*"
+            label="Attach profile picture"
+          ></v-file-input>
+          <v-text-field
+            type="text"
+            error-count=""
+            placeholder="Birthday"
+            label="Birthday"
+            append-icon=""
+            v-model="birthday"
+            readonly
+            color
+          ></v-text-field>
+          <v-date-picker v-model="birthday"></v-date-picker>
+          <v-checkbox label="Agree to terms & conditions"></v-checkbox>
+          <v-btn type="submit" color="primary">Submit</v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Signup',
+  data: () => ({
+    birthday: '',
+    browsers: ['Chrome', 'Firefox', 'Safari', 'Edge', 'Brave']
+  })
+};
+</script>
+
+<style></style>
